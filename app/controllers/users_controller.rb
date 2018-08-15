ef5d0096aab75362b1a616fbb7ed8d0ed3c10345
Kazuya Tsubone <kazuya.tsubone@nulab-inc.com>
2018-08-15 19:19:54 +0900

    scraping

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,71 @@ class UsersController < ApplicationController
   # GET /users
   def index
 
-    # url = ['https://yahoo.co.jp']
-    #解析した画像url入れておく配列を作成する
-    array = []
-    #取得したい画像のあるページのurlを配列に入れておく
-    # urls = %w(url)
-    urls = ['https://yahoo.co.jp']
-    #そしてurlを展開する。展開したurlはNokogiriによって分解され、xpathなりcssで要素を指定できるようになる
-    urls.each do |url|
-      doc = Nokogiri::HTML(open(url))
-    #今回は欲しい画像がimgタグのなかのｓｒｃにあったのでこのようになった。取得したurlを配列に格納しviewで表示するための処理を書いていく。
-      doc.css('img').each do |photo|
-        array << photo[:src]
-      end
+    # # url = ['https://yahoo.co.jp']
+    # #解析した画像url入れておく配列を作成する
+    # array = []
+    # #取得したい画像のあるページのurlを配列に入れておく
+    # # urls = %w(url)
+    # urls = ['https://yahoo.co.jp']
+    # #そしてurlを展開する。展開したurlはNokogiriによって分解され、xpathなりcssで要素を指定できるようになる
+    # urls.each do |url|
+    #   doc = Nokogiri::HTML(open(url))
+    # #今回は欲しい画像がimgタグのなかのｓｒｃにあったのでこのようになった。取得したurlを配列に格納しviewで表示するための処理を書いていく。
+    #   doc.css('img').each do |photo|
+    #     array << photo[:src]
+    #   end
+    # end
+
+    # url = 'https://qiita.com/search?q=ruby'
+
+    # charset = nil
+
+    # array = []
+
+    # html = open(url) do |f|
+    #     charset = f.charset
+    #     f.read
+    # end
+
+    # doc = Nokogiri::HTML.parse(html, nil, charset)
+    # doc.xpath('//h1[@class="searchResult_itemTitle"]').each do |node|
+    #   array << node.css('a').inner_text
+    # end
+
+
+    url = 'https://stocks.finance.yahoo.co.jp/stocks/chart/?code=3092.T&ct=b'
+
+    charset = nil
+
+    stock_info = {
+      time: '',
+      price: '',
+      company: '',
+      change: '',
+      chartUrl: '',
+    }
+
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc.xpath('//div[@id="stockinf"]').each do |node|
+      stock_info[:time] = node.css('dd[@class="yjSb real"]').css('span').inner_text
+      stock_info[:company] = node.css('h1').inner_text
+      stock_info[:price] = node.css('td[@class="stoksPrice"]').inner_text
+      stock_info[:change] = node.css('span[@class="icoUpGreen yjMSt"]').inner_text
+    end
+
+    doc.xpath('//div[@class="padT12 centerFi marB10"]').each do |node|
+      stock_info[:chartUrl] = node.css('img').attribute('src').value
     end
 
 
     @users = User.all
 
-    render json: [@users, array]
+    render json: [@users, stock_info]
   end
 
   # GET /users/1
