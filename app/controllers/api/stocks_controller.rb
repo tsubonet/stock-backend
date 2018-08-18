3b28d5abacb431d213a7e6cb463f4ce55b946d1a
Kazuya Tsubone <kazuya.tsubone@nulab-inc.com>
2018-08-18 17:22:03 +0900

    modify api

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -1,40 +1,44 @@
 class Api::StocksController < ApplicationController
   
-  # GET /stocks
+  require 'open-uri'
+
+  # GET /stocks?code=xxx
   def index
 
-    require 'open-uri'
-
-    url = "https://stocks.finance.yahoo.co.jp/stocks/chart/?code=#{params[:id]}.T&ct=b"
     charset = nil
     stock_info = {}
+    url = "https://stocks.finance.yahoo.co.jp/stocks/chart/?code=#{params[:code]}.T&ct=b"
 
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
-    end
+    begin
+      raise if params[:code].empty?
 
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-
-    doc.xpath('//div[@id="main"]').each do |node|
-      if node.css('div[@class="selectFinTitle yjL"]').inner_text === ' 一致する銘柄は見つかりませんでした'
-        return render json: ['銘柄が見つかりません'], status: :unprocessable_entity
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
       end
-    end
-    
-    doc.xpath('//div[@id="stockinf"]').each do |node|
-      stock_info[:company] = node.css('h1').inner_text
-      stock_info[:time] = node.css('dd[@class="yjSb real"]').css('span').inner_text
-      stock_info[:price] = node.css('td[@class="stoksPrice"]').inner_text
-      stock_info[:change] = node.css('table[@class="stocksTable"]').css('span[2]').inner_text
-    end
 
-    doc.xpath('//div[@class="padT12 centerFi marB10"]').each do |node|
-      stock_info[:chartUrl] = node.css('img').attribute('src').value
-    end
+      doc = Nokogiri::HTML.parse(html, nil, charset)
 
-    render json: stock_info
+      doc.xpath('//div[@id="main"]').each do |node|
+        raise if node.css('div[@class="selectFinTitle yjL"]').inner_text == ' 一致する銘柄は見つかりませんでした'
+      end
+      
+      doc.xpath('//div[@id="stockinf"]').each do |node|
+        stock_info[:company] = node.css('h1').inner_text
+        stock_info[:time] = node.css('dd[@class="yjSb real"]').css('span').inner_text
+        stock_info[:price] = node.css('td[@class="stoksPrice"]').inner_text
+        stock_info[:change] = node.css('table[@class="stocksTable"]').css('span[2]').inner_text
+      end
+
+      doc.xpath('//div[@class="padT12 centerFi marB10"]').each do |node|
+        stock_info[:chartUrl] = node.css('img').attribute('src').value
+      end
+
+      render json: stock_info
+
+    rescue => e
+      render json: { error: '銘柄が見つかりません'}, status: :unprocessable_entity
+    end
 
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, format: 'json' do
-    get '/stocks/:id', to: 'stocks#index'
+    get '/stocks', to: 'stocks#index'
   end
 end
