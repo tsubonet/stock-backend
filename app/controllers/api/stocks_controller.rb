781e321cb28ed8c72e551f947a6e11dca0257d7c
Kazuya Tsubone <kazuya.tsubone@nulab-inc.com>
2018-08-18 16:14:42 +0900

    modify api

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -6,16 +6,8 @@ class Api::StocksController < ApplicationController
     require 'open-uri'
 
     url = "https://stocks.finance.yahoo.co.jp/stocks/chart/?code=#{params[:id]}.T&ct=b"
-
     charset = nil
-
-    stock_info = {
-      time: '',
-      price: '',
-      company: '',
-      change: '',
-      chartUrl: '',
-    }
+    stock_info = {}
 
     html = open(url) do |f|
       charset = f.charset
@@ -23,9 +15,16 @@ class Api::StocksController < ApplicationController
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    doc.xpath('//div[@id="main"]').each do |node|
+      if node.css('div[@class="selectFinTitle yjL"]').inner_text === ' 一致する銘柄は見つかりませんでした'
+        return render json: ['銘柄が見つかりません'], status: :unprocessable_entity
+      end
+    end
+    
     doc.xpath('//div[@id="stockinf"]').each do |node|
-      stock_info[:time] = node.css('dd[@class="yjSb real"]').css('span').inner_text
       stock_info[:company] = node.css('h1').inner_text
+      stock_info[:time] = node.css('dd[@class="yjSb real"]').css('span').inner_text
       stock_info[:price] = node.css('td[@class="stoksPrice"]').inner_text
       stock_info[:change] = node.css('table[@class="stocksTable"]').css('span[2]').inner_text
     end
